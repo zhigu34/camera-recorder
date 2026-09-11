164f18fe31bb3ab1044b01c42f8aba524723b8e5
zhigu34 <zhi.gu@foxmail.com>
2026-09-12 00:49:26 +0800

    ui: load recording playback workspace styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './styles/nvr-theme.css'
+import './styles/recording-browser.css'
 
 import Root from './Root.vue'
 
