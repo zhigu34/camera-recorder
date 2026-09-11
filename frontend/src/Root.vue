e281ac87b0a6f298f0e0564a473322d3a3e0b85d
zhigu34 <zhi.gu@foxmail.com>
2026-09-11 16:14:44 +0800

    feat: expose live preview entry

--- frontend/src/Root.vue
+++ frontend/src/Root.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import App from './App.vue'
 import BatchCamerasView from './BatchCamerasView.vue'
+import PreviewView from './PreviewView.vue'
 import SystemSettingsView from './SystemSettingsView.vue'
 
 const path = window.location.pathname
 const isSettings = path === '/settings'
 const isBatchCameras = path === '/cameras/batch'
+const isPreview = path === '/preview'
 
 function openSettings() {
   window.location.href = '/settings'
@@ -14,14 +16,20 @@ function openSettings() {
 function openBatchCameras() {
   window.location.href = '/cameras/batch'
 }
+
+function openPreview() {
+  window.location.href = '/preview'
+}
 </script>
 
 <template>
   <SystemSettingsView v-if="isSettings" />
   <BatchCamerasView v-else-if="isBatchCameras" />
+  <PreviewView v-else-if="isPreview" />
   <template v-else>
     <App />
     <div class="quick-actions">
+      <el-button type="success" @click="openPreview">实时预览</el-button>
       <el-button type="success" plain @click="openBatchCameras">批量添加摄像头</el-button>
       <el-button type="primary" @click="openSettings">系统设置</el-button>
     </div>
