39f9ae16d3c6908fb827c5708a33f034c864be14
zhigu34 <zhi.gu@foxmail.com>
2026-09-12 00:18:16 +0800

    refactor: normalize root component name

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,7 @@ import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './styles/nvr-theme.css'
 
-import Root from './RootV2.vue'
+import Root from './Root.vue'
 
 document.documentElement.classList.add('dark')
 
