d196d8046cfc5d499b64ae584aff177b41a68066
zhigu34 <zhi.gu@foxmail.com>
2026-09-13 13:54:40 +0800

    feat: export motion models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.camera import Camera
 from app.models.event import Event
 from app.models.health_sample import CameraHealthSample
+from app.models.motion import MotionDetectionSettings, MotionEvent, MotionZone
 from app.models.notification_settings import NotificationSettings
 from app.models.recording import Recording
 from app.models.system_settings import SystemSettings
@@ -14,4 +15,7 @@ __all__ = [
     "CameraHealthSample",
     "NotificationSettings",
     "SystemSettings",
+    "MotionDetectionSettings",
+    "MotionZone",
+    "MotionEvent",
 ]
