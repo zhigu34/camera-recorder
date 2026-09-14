3f8c4bd5f1659f66b07ae487c30e9e1e606eaed1
zhigu34 <zhi.gu@foxmail.com>
2026-09-15 00:15:48 +0800

    fix: parse TypeScript in Vue lint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,26 +1,56 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 import tseslint from 'typescript-eslint'
 
+const globalsForApp = {
+  ...globals.browser,
+  ...globals.es2022,
+}
+
+const typescriptRules = {
+  'no-unused-vars': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+}
+
 export default [
-  {
-    ignores: ['dist/**'],
-  },
+  { ignores: ['dist/**'] },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   {
-    files: ['**/*.{ts,vue}'],
+    files: ['**/*.ts'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2022,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
+      globals: globalsForApp,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: typescriptRules,
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: globalsForApp,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      ...typescriptRules,
       'vue/first-attribute-linebreak': 'off',
       'vue/html-closing-bracket-newline': 'off',
       'vue/html-indent': 'off',
